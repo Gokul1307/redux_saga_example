e9baf8bc9772d13c18cbd1d71eea070624bd4c8a
Gokul1307 <55424219+Gokul1307@users.noreply.github.com>
2021-09-21 14:22:55 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import {watcherSaga} from './saga';
+
+
+
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { reducer } from "./redux";
+import logger from 'redux-logger'
+import createSagaMiddleware from 'redux-saga';
+
+//intitialize the saga middleware
+const sagaMiddleware = createSagaMiddleware()
+
+//store of redux
+let store = createStore(reducer, applyMiddleware(logger, sagaMiddleware));
+
+ReactDOM.render(
+  <Provider store={store}> <App /> </Provider>,
+   
+  document.getElementById('root')
+);
+
+sagaMiddleware.run(watcherSaga);
+
+
+reportWebVitals();
---
+++ src/products_saga.js
@@ -0,0 +1,32 @@
+import {takeLatest, call, put} from 'redux-saga/effects';
+import axios from "axios";
+
+
+//this saga/generator function watches for the trigger of the action mentioned 
+export function* watcherProductsSaga(){
+    yield takeLatest("API_CALL_REQUEST", workerSaga);
+}
+
+//api call
+function fetchDog(){
+    return axios({
+        method: "get",
+        url: "https://fakestoreapi.com/products"
+    });
+}
+
+
+//once the above trigger happens this below gen function gets triggered
+function* workerSaga(){
+    try{
+        const response = yield(call(fetchDog))
+        const dog = response.data.message;
+
+        //dispatch an event that says we got a success response
+
+        yield put({type: "API_CALL_SUCCESS", dog})
+    }
+
+    catch(error){yield put({type: "API_CALL_FAILURE", error})}
+    
+}
